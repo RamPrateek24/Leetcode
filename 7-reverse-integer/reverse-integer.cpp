ab234767661edf7e95b16f128250778b5f3f2b94
Ram Prateek <131789883+RamPrateek24@users.noreply.github.com>
2025-01-21 19:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.48%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string s = to_string(x);
+        if (s[0] == '-') {
+            std::reverse(s.begin() + 1, s.end());
+        } else {
+            std::reverse(s.begin(), s.end());
+        }
+        try {
+            return stoi(s);
+        } catch (out_of_range&) {
+            return 0;
+        }
+    }
+};
