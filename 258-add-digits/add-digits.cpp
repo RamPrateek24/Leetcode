1588d81d02567d8d0b1f783322efef197f4e86b0
Ram Prateek <131789883+RamPrateek24@users.noreply.github.com>
2025-02-04 14:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (45.65%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int k,sum = 0;
+        while(num){
+           sum+= num%10;
+           num /= 10;
+        }
+        if(sum > 9) return addDigits(sum);
+        return sum;
+    }
+};
