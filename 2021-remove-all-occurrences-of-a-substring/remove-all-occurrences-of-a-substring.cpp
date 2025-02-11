6ae7de9719f5fe16ff7f05a3c67b543a568c4b11
Ram Prateek <131789883+RamPrateek24@users.noreply.github.com>
2025-02-11 22:33:09 +0530

    Time: 3 ms (32.43%) | Memory: 9.4 MB (34.81%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+       int n = s.find(part);
+    
+    while (n != string::npos) {
+        s.erase(n, part.length());
+        n = s.find(part); // Find again after erasing
+    }
+    
+    return s; 
+    }
+};
