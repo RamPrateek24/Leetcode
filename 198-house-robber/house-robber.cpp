d1d2d128fa56e71fbe6e9b6c6dd704fc7eb2904a
MANTRIPRAGADA RAM PRATEEK <131789883+RamPrateek24@users.noreply.github.com>
2025-08-31 14:27:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (64.50%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,13 +4,16 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, -1);
-        dp[0] = nums[0];
+        int prev = nums[0];
+        int prev2 = 0;
         for(int i=1; i<n; i++){
            int pick = nums[i];
-           if(i>1) pick += dp[i-2];
-           int npick = dp[i-1];
-           dp[i] = max(pick, npick); 
+           if(i>1) pick += prev2;
+           int npick = prev;
+           int curr = max(pick, npick); 
+           prev = curr;
+           prev2 = npick;
         }
-        return dp[n-1];
+        return prev;
     }
 };
