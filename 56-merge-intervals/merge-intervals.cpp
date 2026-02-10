acd37dc39a2af12b206a0c769567f082c1cfb7ec
MANTRIPRAGADA RAM PRATEEK <131789883+RamPrateek24@users.noreply.github.com>
2026-02-10 11:42:45 +0530

    Time: 4 ms (66.84%) | Memory: 23.9 MB (58.81%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> res;
-        if(intervals.size() == 0) return res;
         sort(intervals.begin(), intervals.end());
-        vector<int> temp = intervals[0];
-        for(auto it:intervals){
-            if(it[0] <= temp[1]){
-                temp[1] = max(it[1], temp[1]);
+        for(int i=0; i<intervals.size(); i++){
+            if(res.empty() || res.back()[1] < intervals[i][0]){
+                res.push_back(intervals[i]);
             }
             else{
-                res.push_back(temp);
-                temp = it;
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
             }
         }
-        res.push_back(temp);
         return res;
     }
 };
