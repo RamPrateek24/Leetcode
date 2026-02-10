14cc46daa532c88ad44f5d805ec286e2c40636ee
MANTRIPRAGADA RAM PRATEEK <131789883+RamPrateek24@users.noreply.github.com>
2026-02-10 08:49:50 +0530

    Time: 4 ms (53.93%) | Memory: 15.9 MB (5.51%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]] = i;
+        }
+        for(int i=0; i<nums.size(); i++){
+            int req = target-nums[i];
+            if(mpp.find(req) != mpp.end() && mpp[req] != i){
+                return {i, mpp[req]};
+            }
+        }
+        return {};
+    }
+};
